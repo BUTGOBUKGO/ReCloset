f17d2a0926855e9ca9bf633e36cbaeb82fc6bd92
seoan <seoan@seoanlecbookair>
2020-01-15 16:15:14 +0900

    20200115 16:14 Seoan

--- ReCloset/src/main/java/com/kh/recloset/myPage/controller/MyPageController.java
+++ ReCloset/src/main/java/com/kh/recloset/myPage/controller/MyPageController.java
@@ -1,8 +1,18 @@
 package com.kh.recloset.myPage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.recloset.myPage.model.service.MyPageService;
 
@@ -15,6 +25,47 @@ public class MyPageController {
 	@RequestMapping("/myPage.do")
 	public String myPage() {
 		return "/myPage/myPage";
+	}
+
+	// 주문내역
+	@RequestMapping ("/myList.do")
+	public ModelAndView myList(HttpSession session, ModelAndView mav, Model model){
+		
+		
+		// session에 저장된 userId
+		String userId = (String)session.getAttribute("userId");
+		
+		// 주문내역조회에서 보여줄 주문수
+		int numPerOrder = 5;
+		
+		System.out.println(numPerOrder);
+		
+		//Map<String, Object> map = new HashMap<String, Object>();
+		List<Map<String,Object>> list = OrderService.selectList(numPerOrder);
+		
+		/*
+		// 주문 정보
+		List<OrderList> = orderService.listOrder(userId);
+		// 주문 총액
+		int totalCost = orderService.totoalCost(userId);
+		
+		
+		// ??
+		map.put("list", list);
+		map.put("totalCost" totalCost);
+		mav.setViewName("myPage/myList");
+		mav.addObject("map", map);
+		return mav;
+		
+		
+		// ????
+	 	model.addAttribute("list", list)
+		     .addAttribute("totalCost", totalCost)
+		     .addAttribute("numPerOrder", numPerOrder)
+		
+		*/
+
+		return mav;
 	}
 	
 	
---
+++ ReCloset/src/main/java/com/kh/recloset/myPage/model/vo/myPage.java
@@ -0,0 +1,9 @@
+package com.kh.recloset.myPage.model.vo;
+
+import java.io.Serializable;
+
+public class myPage implements Serializable{
+	
+	
+
+}
